a4926dedd0b5aae4766b1598fa2398ee5a3a49f4
LucasCta <lucas.ct.araujo@gmail.com>
2023-07-30 16:35:15 -0300

    Adding gift-box section

--- src/app/Products/page.tsx
+++ src/app/Products/page.tsx
@@ -15,14 +15,14 @@ export default function Products() {
       className={`flex flex-col justify-center items-center py-16 ${flower.className}`}
     >
       <div className="flex items-center gap-3">
-        <hr className="my-8 w-32 h-[4px] bg-[#EEB960] border-0" />
-        <div className="grid items-center">
-          <p className={`text-4xl text-[#46362B]`}>Arranjos de Flores</p>
-          <p className={`text-4xl text-[#46362B]`}>Elegância Natural</p>
+        <hr className="my-8 w-64 h-[4px] bg-[#EEB960] border-0" />
+        <div className="grid items-center mx-20">
+          <p className={`text-center text-4xl text-[#46362B]`}>Arranjos de Flores</p>
+          <p className={`text-center text-4xl text-[#46362B]`}>Elegância Natural</p>
         </div>
-        <hr className="my-8 w-32 h-[4px] bg-[#EEB960] border-0" />
+        <hr className="my-8 w-64 h-[4px] bg-[#EEB960] border-0" />
       </div>
-      <p className="text-2xl w-2/3 font-semibold py-7">
+      <p className="text-center text-2xl w-2/3 font-semibold py-7 mb-10">
         Nossos arranjos de flores são verdadeiras obras de arte da natureza. Com
         cuidado artesanal, selecionamos flores frescas e harmonizamos cores e
         formas para criar composições únicas. Seja para casamentos, aniversários
@@ -79,6 +79,41 @@ export default function Products() {
           </div>
         </div>
       </div>
+      <div className="mt-10 flex items-center gap-3">
+        <hr className="my-8 w-64 h-[4px] bg-[#EEB960] border-0" />
+        <p className={`mx-20 text-center text-5xl text-[#46362B]`}>Gift Boxes</p>
+        <hr className="my-8 w-64 h-[4px] bg-[#EEB960] border-0" />
+      </div>
+      <p className="text-center text-2xl w-2/3 font-semibold py-7 mb-10">
+      Nossas caixas de presentes estão disponíveis durante as estações de outono/inverno. Todas as caixas de presente são cuidadosamente selecionadas, contendo produtos de nossos parceiros locais/pequenos negócios
+      </p>
+      <div className="grid grid-cols-1 md:grid-cols-2 w-3/4 justify-between">
+        <div className="flex text-center items-center justify-center flex-col">
+          <Image src="/Rectangle 2.png" width={400} height={400} alt="01" />
+          <div className={`pt-4 text-[#332B25] ${roboto.className}`}>
+            <p className={`text-xl`}>“SWEETER THAN HONEY”</p>
+            <p className={`text-xl`}>FALL GIFT BOX</p>
+          </div>
+          <div
+            className={`py-4 text-lg text-[#332B25] opacity-80 ${roboto.className}`}
+          >
+            R$60.00
+          </div>
+        </div>
+        <div className="flex text-center items-center justify-center flex-col">
+          <Image src="/Rectangle 3.png" width={400} height={400} alt="01" />
+          <div className={`pt-4 text-[#332B25] ${roboto.className}`}>
+            <p className={`text-xl`}>“STARRY EYED”</p>
+            <p className={`text-xl`}>WINTER GIFT BOX</p>
+          </div>
+          <div
+            className={`py-4 text-lg text-[#332B25] opacity-80 ${roboto.className}`}
+          >
+            R$45.00
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
+
